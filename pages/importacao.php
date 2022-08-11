29a3fa10fe431dd4c9f89ddfaac49e706dfc8b76
Felipe Garcia <61507578+felipeggarcia@users.noreply.github.com>
2022-08-11 18:16:02 -0300

    criação da página onde é carregada o arquivo de texto

---
+++ pages/importacao.php
@@ -0,0 +1,28 @@
+
+<?php include '../lib/functions.php';?>
+<html>
+<?php include 'head.html';?>
+<body>
+<?php include 'header.html';?>
+
+<section >
+    <form action="../api/imp_cnab.php" method="post"  enctype="multipart/form-data">
+        <p></p>
+        <h3>Importação CNAB</h3>
+        <div class="import-form">
+            <input type="file" id="file-cnab" name="file-cnab"  accept=".txt">
+            <br>
+            <input type="submit" value="Importar Dados">
+        </div>
+    </form>
+    <div class="container">
+
+    </div>
+</section>
+
+
+<script async type="text/javascript" src="../script.js"></script>
+
+
+</body>
+</html>
