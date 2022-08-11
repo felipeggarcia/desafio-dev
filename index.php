30a1df8439d25c48ffbb6f6abbddacc6a246471b
Felipe Garcia <61507578+felipeggarcia@users.noreply.github.com>
2022-08-11 18:17:52 -0300

    primeira versão do index do desafio

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Desafio Dev</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<section class="navigation w3-metro-light-blue w3-container">
+    <button onclick="goToTop()" id="back-to-top" title="Go to top"><i class="fa fa-toggle-up"></i></button>
+    <nav class="nav-container">
+        <div class="logo w3-padding-large">
+            <p>DESAFIODEV</p>
+        </div>
+        <div class="links">
+            <a href="pages/sobre.php" >SOBRE</a>-
+            <a href="pages/importacao.php" >IMPORTAÇÃO</a>-
+            <a href="pages/tabelas.php" >TABELAS</a>-
+            <a href="pages/documentacao.php" >DOCUMENTAÇÃO</a>
+        </div>
+    </nav>
+</section>
+
+<section class="hero w3-padding-large w3-container">
+    <p>Bem vindo ao DESAFIO DEV desenvolvido por Felipe Garcia</p>
+    <p>Proposto por <img src="logo_bycoders_.svg" alt="bycoders_" width="150px"></p>
+</section>
+<script async type="text/javascript" src="script.js"></script>
+</body>
+</html>
