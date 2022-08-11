d73cb6d23959d616c755e64b002e8fc6b993f029
Felipe Garcia <61507578+felipeggarcia@users.noreply.github.com>
2022-08-11 18:16:55 -0300

    adição de funções de validação de hora e cpf

--- lib/functions.php
+++ lib/functions.php
@@ -1,17 +1,49 @@
 <?php
-function decodificarCNAB($strCnab)
+function decodificarCNAB($strCnab): array
 {
     $arrCnab = explode(PHP_EOL,$strCnab);
 
     foreach ($arrCnab as $key =>$item){
-        $arrDecodedCnab[$key]['tipo'] = mb_substr($item, 0, 1);
-        $arrDecodedCnab[$key]['data'] = mb_substr($item, 1, 9);
-        $arrDecodedCnab[$key]['valor'] = mb_substr($item, 9, 10)/100;
-        $arrDecodedCnab[$key]['cpf'] = mb_substr($item, 19, 11);
-        $arrDecodedCnab[$key]['cartao'] = mb_substr($item, 30, 12);
-        $arrDecodedCnab[$key]['hora'] = mb_substr($item, 42, 6);
-        $arrDecodedCnab[$key]['dono_loja'] = mb_substr($item, 48, 14);
-        $arrDecodedCnab[$key]['nome_loja'] = mb_substr($item, 62, 19);
+        if(mb_substr($item, 0, 1)){//validar os dados aqui
+            $arrDecodedCnab[$key]['tipo'] = mb_substr($item, 0, 1);
+            $arrDecodedCnab[$key]['data']['ano'] = mb_substr($item, 1, 4);
+            $arrDecodedCnab[$key]['data']['mes'] = mb_substr($item, 5, 2);
+            $arrDecodedCnab[$key]['data']['dia'] = mb_substr($item, 7, 2);
+            $arrDecodedCnab[$key]['valor'] = mb_substr($item, 9, 10);
+            $arrDecodedCnab[$key]['cpf'] = mb_substr($item, 19, 11);
+            $arrDecodedCnab[$key]['cartao'] = mb_substr($item, 30, 12);
+            $arrDecodedCnab[$key]['hora']= mb_substr($item, 42, 2).':'.mb_substr($item, 44, 2).':'.mb_substr($item, 46, 2);
+            $arrDecodedCnab[$key]['dono_loja'] = mb_substr($item, 48, 14);
+            $arrDecodedCnab[$key]['nome_loja'] = mb_substr($item, 62, 19);
+        }
+
     }
     return $arrDecodedCnab;
+}
+function validaCPF($cpf): bool
+{
+    $cpf = preg_replace('/D/is', '', $cpf);
+
+    if (strlen($cpf) != 11) {
+        return false;
+    }
+
+    if (preg_match('/(\d)\1{10}/', $cpf)) {
+        return false;
+    }
+
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function validaHora($hora){
+    return  preg_match("#([0-1]\d|2[0-3]):[0-5]\d:[0-5]\d#", $hora);
 }
