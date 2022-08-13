7271903b54c8d6b5caaa1d653e9677f5ae57e083
Felipe Garcia <61507578+felipeggarcia@users.noreply.github.com>
2022-08-12 22:38:14 -0300

    criação da api para o parsear e inserir os dados no banco de dados

---
+++ lib/api/imp_cnab.php
@@ -0,0 +1,75 @@
+<?php
+require_once '../models/database.php';
+require_once '../functions.php';
+//voltar aqui depois
+//o que ainda falta:
+//redirecionar a página para tabelas, fazendo um relatório do que foi feito
+// gerar erro de inserção no bd, dados inválidos e dados duplicados
+//e tmb o que deu certo
+// :)
+$objJson=json_decode(file_get_contents('php://input'));
+if(isset($objJson->string_cnab)){
+    $con = new Database();
+    $con=$con->connect();
+//
+    $arrCnab=decodificarCNAB($objJson->string_cnab);
+    $consulta = $con->query("SELECT * FROM tipo_transicao");
+    $arrTipo=[];
+    while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+        $arrTipo[]=$linha['tipo'];
+    }
+    $countErros=0;
+    $countSucessos=0;
+    foreach ($arrCnab as $key =>$item){
+        if (in_array($item['tipo']??'', $arrTipo)&&
+            checkdate ( $item['data']['mes']??0, $item['data']['dia']??0, $item['data']['ano']??0 )&&
+            is_numeric($item['valor']??'')&&
+            validaCPF($item['cpf']??'')
+            &&validaHora($item['hora']??'')
+            ){
+            $querySelect="SELECT id FROM transicoes
+            WHERE tipo = '".$item['tipo']."'
+            AND  data = '".$item['data']['ano']."-".$item['data']['mes']."-".$item['data']['dia']."'
+            AND  valor='".$item['valor']."'
+            AND  cpf='".$item['cpf']."'
+            AND  cartao ='".$item['cartao']."'
+            AND  hora ='".$item['hora']."'
+            AND  dono_loja ='".trim($item['dono_loja'])."'
+            AND  nome_loja ='".trim($item['nome_loja'])."'
+            ";
+            $consulta=$con->query($querySelect);
+            if(!$consulta->fetch(PDO::FETCH_ASSOC)){
+                $queryInsert="INSERT INTO `transicoes`
+                (`id`, `tipo`, `data`, `valor`, `cpf`, `cartao`, `hora`, `dono_loja`, `nome_loja`)VALUES
+                (NULL,
+                 '".$item['tipo']."',
+                 '".$item['data']['ano']."-".$item['data']['mes']."-".$item['data']['dia']."',
+                 '".$item['valor']."',' ".$item['cpf']."', '".$item['cartao']."', '".$item['hora']."',
+                 '".trim($item['dono_loja'])."','".trim($item['nome_loja'])."')";
+                if($con->query($queryInsert)){
+                    $countSucessos++;
+                }else{
+                    $countErros++;// erro de conecção com o banco de dados
+                }
+
+            }else{
+                $countErros++;;//dado duplicado
+            }
+
+        }else{
+            $countErros++;//não é dado válido
+        }
+    }
+    $response='';
+    if($countErros>0&&$countSucessos>0)$response = '{"error":1, "sucess":1,"message":';
+    if($countErros==0&&$countSucessos>0)$response =  '{"error":0, "sucess":1,"message":';
+    if($countErros>0&&$countSucessos==0)$response =  '{"error":1, "sucess":0,"message":';
+    if($countErros==0&&$countSucessos==0)$response =  '{"error":0, "sucess":0,"message":';
+$response.='"Foram feitas '.($countErros+$countSucessos).' tentativas de inserção de dados, onde '.$countSucessos.' foram um sucesso e '.$countErros.' falharam."}';
+    print_r(($response));
+
+ }else{
+    echo '{"error":1, "message":"JSON enviado incorretamente"}';
+}
+
+
--- pages/importacao.php
+++ pages/importacao.php
@@ -6,7 +6,7 @@
 <?php include 'header.html';?>
 
 <section >
-    <form action="../api/imp_cnab.php" method="post"  enctype="multipart/form-data">
+    <form action="importacao.php" method="post" enctype="multipart/form-data">
         <p></p>
         <h3>Importação CNAB</h3>
         <div class="import-form">
@@ -15,7 +15,35 @@
             <input type="submit" value="Importar Dados">
         </div>
     </form>
+<?php
+    if(isset($_FILES['file-cnab'])){
+        $myfile = fopen($_FILES['file-cnab']['tmp_name'], "r") ;
+        $strCnab=fread($myfile,filesize($_FILES['file-cnab']['tmp_name']));
+        fclose($myfile);
+        $url = "http://$_SERVER[HTTP_HOST]/desafio-dev/lib/api/imp_cnab.php";
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
+        $headers = array(
+            "Content-Type: application/json",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        $data='{"string_cnab":'.json_encode($strCnab).'}';
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        $objResp=json_decode($resp);
+        if($objResp->error&&$objResp->sucess) echo '<div class="message warning">';
+        if(!$objResp->error&&$objResp->sucess) echo '<div class="message sucess">';
+        if($objResp->error&&!$objResp->sucess) echo '<div class="message error">';
+
+        echo $objResp->message ;
+        echo '</div>';
+    }
+?>
 </section>
 
 
